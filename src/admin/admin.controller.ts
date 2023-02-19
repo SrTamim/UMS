038521d8bce17cbcb7a4db56cd5f76e8dda70cd7
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2023-02-19 22:47:01 +0600

    Update admin.controller.ts

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,92 +1,87 @@
-import { Body, Controller, Delete, Get, Param, Put, Query, Post } from "@nestjs/common";
-import { AdminService } from "./adminservice.service";
-import { AdminForm } from "./adminform.dto";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  Query,
+  Post,
+} from '@nestjs/common';
+import { AdminService } from './adminservice.service';
+import { AdminForm } from './adminform.dto';
 
+@Controller('/admin')
+export class AdminController {
+  constructor(private adminService: AdminService) {}
 
-@Controller("/admin")
-export class AdminController
-{ 
-  constructor(private adminService: AdminService){}
+  @Get('/index')
+  getAdmin(): any {
+    return this.adminService.getIndex();
+  }
+  @Get('/findadmin/:id')
+  getAdminByID(@Param('id') id: number): any {
+    return this.adminService.getAdminByID(id);
+  }
 
-  @Get("/index")
-    getAdmin(): any { 
-        return this.adminService.getIndex();
-    }
-    @Get("/findadmin/:id")
-    getAdminByID(@Param("id") id:number,): any {
-      return this.adminService.getAdminByID(id);
-    }
+  @Get('/findadmin')
+  getAdminByIDName(@Query() qry: any): any {
+    return this.adminService.getAdminByIDName(qry);
+  }
 
-    @Get("/findadmin")
-    getAdminByIDName(@Query() qry:any): any {
-      return this.adminService.getAdminByIDName(qry);
-    }  
-  
-    @Put("/updateadmin/")
-    updateAdmin( 
-      @Body("name") name:string, 
-      @Body("id") id:number
-      ): any {
+  @Put('/updateadmin/')
+  updateAdmin(@Body('name') name: string, @Body('id') id: number): any {
     return this.adminService.updateAdmin(name, id);
-    }
-    
-    @Put("/updateadmin/:id")
-  updateAdminbyid( 
-      @Body("name") name:string, 
-      @Param("id") id:number
-      ): any {
-    return this.adminService.updateAdminbyid(name,id);
-    }
+  }
 
-    @Delete("/deleteadmin/:id")
-  deleteAdminbyid( 
-     @Param("id") id:number
-      ): any {
+  @Put('/updateadmin/:id')
+  updateAdminbyid(@Body('name') name: string, @Param('id') id: number): any {
+    return this.adminService.updateAdminbyid(name, id);
+  }
+
+  @Delete('/deleteadmin/:id')
+  deleteAdminbyid(@Param('id') id: number): any {
     return this.adminService.deleteAdminbyid(id);
-    }
+  }
 
-    //---------------------
+  //---------------------
 
-    @Get("/findCourse")
-    getCourseByID(@Query() qry:any): any {
-      return this.adminService.getCourseByID(qry);
-    }  
+  @Get('/findCourse')
+  getCourseByID(@Query() qry: any): any {
+    return this.adminService.getCourseByID(qry);
+  }
 
-    @Put("/updateCourse/:id")
-    updateCoursebyid( 
-        @Body("Cname") Cname:string, 
-        @Param("Cid") Cid:number
-        ): any {
-      return this.adminService.updateCoursebyid(Cname,Cid);
-      }
+  @Put('/updateCourse/:id')
+  updateCoursebyid(
+    @Body('Cname') Cname: string,
+    @Param('Cid') Cid: number,
+  ): any {
+    return this.adminService.updateCoursebyid(Cname, Cid);
+  }
 
-      //----------------------
-      @Post("/insertRoom")
-      insertRoom(@Body() mydto:AdminForm): any {
-        return this.adminService.insertRoom(mydto);
-      }
+  //----------------------
+  @Post('/insertRoom')
+  insertRoom(@Body() mydto: AdminForm): any {
+    return this.adminService.insertRoom(mydto);
+  }
 
-      @Put("/updateRoom/:Rid")
-    updateRoombyid( 
-        @Param("Rid") Rid:number
-        ): any {
-      return this.adminService.updateRoombyid(Rid);
-      }
+  @Put('/updateRoom/:Rid')
+  updateRoombyid(@Param('Rid') Rid: number): any {
+    return this.adminService.updateRoombyid(Rid);
+  }
 
-      //-------------
-      @Post("/insertNotice")
-      insertNotice(@Body() mydto:AdminForm): any {
-        return this.adminService.insertNotice(mydto);
-      }
+  //-------------
+  @Post('/insertNotice')
+  insertNotice(@Body() mydto: AdminForm): any {
+    return this.adminService.insertNotice(mydto);
+  }
 
-      //----------
-      @Put("/updateGrade/:id")
-      updateGradebyid( 
-          @Body("StudentName") Sname:string, 
-          @Param("Sid") Sid:number
-          ): any {
-        return this.adminService.updateGradebyid(Sname,Sid);
-        }
-
-
+  //----------
+  @Put('/updateGrade/:id')
+  updateGradebyid(
+    @Body('StudentName') Sname: string,
+    @Param('Sid') Sid: number,
+  ): any {
+    return this.adminService.updateGradebyid(Sname, Sid);
+  }
 }
