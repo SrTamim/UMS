a9318273ff810c5b099a5c776ad2ff03fdf496c8
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2023-02-27 23:44:04 +0600

    p

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/adminmodule.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FacultyModule } from './Faculty/faculty.module';
 
 @Module({
-  imports: [AdminModule, TypeOrmModule.forRoot(
-   { type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'admin',
-    database: 'UMS',
-    autoLoadEntities: true,
-    synchronize: true,
-  }
-  ),],
+  imports: [
+    AdminModule,
+    FacultyModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Advweb3',
+      database: 'UMS',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
