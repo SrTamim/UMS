4a0b9927a544ef876585bd3bea24c19222554c16
Md. Fahim Alam <kfahim2280@gmail.com>
2023-05-15 04:26:03 +0600

    new

--- src/Faculty/Services/facultyservice.service.ts
+++ src/Faculty/Services/facultyservice.service.ts
@@ -26,21 +26,6 @@ export class FacultyService {
     faculty.Fnum = facultydto.Fnum;
     faculty.dob = facultydto.dob;
     faculty.Fsal = facultydto.Fsal;
-
-    // for (let notice of facultydto.facultyNotices) {
-    //   const facultyNotice = new FacultyNotice();
-    //   facultyNotice.subject = notice.subject;
-    //   facultyNotice.Details = notice.Details;
-    //   faculty.facultyNotices.push(facultyNotice);
-    // }
-
-    for(let i=0; i<facultydto?.facultyNotices.length; i++){
-      const facultyNotice = new FacultyNotice();
-      //facultyNotice.subject = facultydto?.facultyNotices[i].subject;
-      //facultyNotice.Details = facultydto?.facultyNotices[i].Details;
-      //faculty.facultyNotices.push(facultyNotice);
-    }
-
     return this.facultyRepo.save(faculty);
   }
 
